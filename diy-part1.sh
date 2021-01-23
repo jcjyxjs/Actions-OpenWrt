c2f056a52021ebc079478bee1e4fd84b14015100
limi00 <75186809+limi00@users.noreply.github.com>
2021-01-23 11:43:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,18 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git diy https://github.com/281677160/openwrt-package' feeds.conf.default
+sed -i '$a src-git project https://github.com/project-openwrt/packages' feeds.conf.default
 
 rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
+
+# 增加 SSID 2.5G
+sed -i '/channel="11"/a\\t\tssid="BJYS20196688"' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# 增加 SSID 5.0G
+sed -i '/channel="36"/a\\t\t\tssid="BJYS20196688-5HZ"' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# 修改默认 SSID
+sed -i 's/OpenWrt/${ssid}/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# 修改默认密钥
+sed -i 's/none/psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# 增加默认WIFI密码
+sed -i '/set wireless.radio${devidx}.hwmode=11/a\\t\t\tset wireless.radio${devidx}.country=CN\n\t\t\tset wireless.radio${devidx}.legacy_rates=1\n\t\t\tset wireless.radio${devidx}.mu_beamformer=1' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i '/set wireless.default_radio${devidx}.encryption=psk2/a\\t\t\tset wireless.default_radio${devidx}.key=password\n\t\t\tset wireless.default_radio${devidx}.ieee80211k=1' package/kernel/mac80211/files/lib/wifi/mac80211.sh
