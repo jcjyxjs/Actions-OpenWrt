92fe82a8007aa260b61b3d9e913be7a3060224d6
limi00 <75186809+limi00@users.noreply.github.com>
2021-01-23 20:56:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,5 +17,8 @@ sed -i 's/OpenWrt/XiaoMi_R3G/g' package/base-files/files/bin/config_generate
 # Modify WiFi ON
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
+# Modify default SSID
+# sed -i 's/ssid=OpenWrt/ssid=Panzy/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
