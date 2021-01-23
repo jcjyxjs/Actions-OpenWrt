07f7a2c2117e070603485af672a95e15589940be
limi00 <75186809+limi00@users.noreply.github.com>
2021-01-23 21:26:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,12 +14,6 @@ sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_genera
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i 's/OpenWrt/XiaoMi_R3G/g' package/base-files/files/bin/config_generate
 
-#添加自定义插件链接（自己想要什么就github里面搜索然后添加）
-git clone -b 18.06 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge  #主题-edge-动态登陆界面
-git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom  #透明主题
-git clone -b https://github.com/rufengsuixing/luci-app-usb3disable  #usb3disable
-git clone https://github.com/garypang13/luci-app-eqos package/luci-app-eqos  #内网IP限速工具
-
 # Modify WiFi ON
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
